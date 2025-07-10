b4fbbac9460b6ad9eea2615fc5b41b46e053582c
Ethen.exe <leepakyiu@gmail.com>
2025-07-10 19:14:49 -0400

    deleting wrongfully put -final-

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -4,7 +4,7 @@ package use_case.logout;
  * The Input Data for the Logout Use Case.
  */
 public class LogoutInputData {
-    private final String username;
+    private String username;
 
     public LogoutInputData(String username) {
         // TODO: save the current username in an instance variable and add a getter.
